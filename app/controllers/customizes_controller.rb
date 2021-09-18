8563c39333aacbba92fdcf30f2a25baa7959d13e
RunakoMasuda <88620934+RunakoMasuda@users.noreply.github.com>
2021-09-18 17:57:46 +0900

    customize_create画面の実装

--- app/controllers/customizes_controller.rb
+++ app/controllers/customizes_controller.rb
@@ -1,8 +1,33 @@
 class CustomizesController < ApplicationController
   def index
+    #トップページ
   end
  
   def new
+    #ログインユーザーがcustomizeを持っている場合はそれを表示させたい
+    @customize = Customize.new
+    5.times{ @customize.details.build}
+  end
+
+  def create
+   @customize = Customize.new(customize_params)
+    if @customize.save
+      #redirect_to customizes_path
+    else
+      render :new
+    end
+
+  end
+
+  def show
+    @customize = Customize.find(params[:id])
   end
   
+  private
+
+  def customize_params
+    #binding.pry
+    params.require(:customize).permit(:theme,details_attributes:[:detail]).merge(user_id: current_user.id)
+  end
+
 end
