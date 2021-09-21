303d1dba8107002b385a97f6560dcfe4a450cce0
RunakoMasuda <88620934+RunakoMasuda@users.noreply.github.com>
2021-09-21 14:23:01 +0900

    yesno_機能実装

---
+++ app/javascript/yesno.js
@@ -0,0 +1,16 @@
+function yesno (){
+  const submit = document.getElementById("yes_no_btn");
+  submit.addEventListener("click", () => {
+    const imagelist = new Array(
+      "/assets/no.jpeg",
+      "/assets/yes.jpeg"
+       );
+      let img = new Image();
+      const imageNo = Math.floor(Math.random() * imagelist .length);
+      img.src = imagelist[imageNo];
+      console.log(img.src);
+      document.write("<img src =" + img.src + ">");
+ });
+};
+  
+window.addEventListener('load', yesno);
